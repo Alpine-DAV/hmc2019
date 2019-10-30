0e6c909bfd72ad20e56bee82e53a46377e9f2a99
David Sobek <davidfsobek@gmail.com>
2019-10-29 22:10:20 -0700

    Added some comments

--- point_locations.sh
+++ point_locations.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# To use, just place in your devil_ray/build/utilities/furnace/
+# directory where point_location and point_config.yaml is.
+# Usage: ./point_locations.sh NUM_OF_TRIALS
+set -e
+
 DRAY_OUPUT_PREFIX=${DRAY_LOG_PREFIX:-dray_data}
 
 for ((i=1; i <= ${1:?}; i++)) 
