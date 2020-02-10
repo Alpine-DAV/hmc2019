9818775598f78c981aced3e1a050220ea3036ae1
davidfsobek <davidfsobek@gmail.com>
2020-02-09 18:45:06 -0800

    made the point_locations script a bit better

--- point_locations.sh
+++ point_locations.sh
@@ -2,14 +2,14 @@
 
 # To use, just place in your devil_ray/build/utilities/furnace/
 # directory where point_location and point_config.yaml is.
-# Usage: ./point_locations.sh NUM_OF_TRIALS
+# Usage: ./point_locations.sh START_INDEX END_INDEX /optional/output/dir
 set -e
 
 DRAY_OUPUT_PREFIX=${DRAY_LOG_PREFIX:-dray_data}
 
-for ((i=1; i <= ${1:?}; i++)) 
+for ((i=${1:?}; i <= ${2:?}; i++)) 
 do
     echo "Build Trial #$i"
     ./point_location point_config.yaml
-    mv ${DRAY_OUPUT_PREFIX}_0.yaml ${DRAY_OUPUT_PREFIX}_${i}.yaml
+    mv ${DRAY_OUPUT_PREFIX}_0.yaml ${3}${DRAY_OUPUT_PREFIX}_${i}.yaml
 done
